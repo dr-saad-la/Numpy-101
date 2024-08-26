008994bd195a159422bce4ea5b2956af6dca2df0
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2024-08-26 04:28:18 +0100

    Add script file

---
+++ scripts/numpy_version.py
@@ -0,0 +1,13 @@
+"""Check numpy version."""
+__author__ = "Dr. Saad Laouadi"
+__copyright__ = "All rights reserved to the author"
+
+import numpy as np
+
+
+def print_version():
+    print(np.__version__)
+
+
+if __name__ == "__main__":
+    print_version()
